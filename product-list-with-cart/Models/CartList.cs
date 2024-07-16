62b6de7e3b89d1fa8934583bd40dfd48ba171c5d
Raul Hernandez <rehcgt@outlook.com>
2024-07-16 14:59:07 -0600

    changing cart to work as a component

---
+++ product-list-with-cart/Models/CartList.cs
@@ -0,0 +1,13 @@
+﻿
+public class CartList
+{
+    public Guid CartId { get; set; }
+    public List<CartItem> Items { get; set; }
+}
+
+public class CartItem
+{
+    public string ProductName { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+}
