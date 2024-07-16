c3f1c53578099f7054d3ae45fea3273872e38275
Raul Hernandez <rehcgt@outlook.com>
2024-07-16 09:25:42 -0600

    Adding components and mocking up the products view

---
+++ product-list-with-cart/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace product_list_with_cart.Models
+{
+    public class Product
+    {
+        [JsonProperty("image")]
+        public ImageUrls Image { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class ImageUrls
+    {
+        [JsonProperty("thumbnail")]
+        public string Thumbnail { get; set; }
+        [JsonProperty("mobile")]
+        public string Mobile { get; set; }
+        [JsonProperty("tablet")]
+        public string Tablet { get; set; }
+        [JsonProperty("desktop")]
+        public string Desktop { get; set; }
+    }    
+}
