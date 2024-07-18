4deace61406b38fcd1df2218951c32ff9baf9031
Raul Hernandez <rehcgt@outlook.com>
2024-07-17 18:41:05 -0600

    adding confirm dialog

--- product-list-with-cart/Models/CartItem.cs
+++ product-list-with-cart/Models/CartItem.cs
@@ -3,6 +3,7 @@ public class CartItem
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public string ProductName { get; set; }
+    public string Thumbnail { get; set; }
     public int Quantity { get; set; }
     public decimal UnitPrice { get; set; }
 }
